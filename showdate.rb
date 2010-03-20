ba79f14be345d8326b4d2c95f72fd2fe05710674
Joe Auricchio <jauricchio@gmail.com>
2010-03-20 14:20:05 -0700

    Move awkward global vars into show_date()

--- showdate.rb
+++ showdate.rb
@@ -2,11 +2,11 @@
 
 require 'date'
 
-$rerun_start = Date.parse("2010-03-19")
-$series_start = Date.parse("2006-03-17")
-
 def show_date(today)
-    $series_start + (today - $rerun_start)
+  rerun_start = Date.parse("2010-03-19")
+  series_start = Date.parse("2006-03-17")
+
+  series_start + (today - rerun_start)
 end
 
 if __FILE__ == $0 then
