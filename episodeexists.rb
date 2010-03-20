2d6452ff3238a50cbf25c3ddf579560a9d99ed8c
Joe Auricchio <jauricchio@gmail.com>
2010-03-20 03:17:11 -0700

    episode_exists(date) - check with zefrank.com

---
+++ episodeexists.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+require 'net/http'
+require 'uri'
+
+require 'showdate'
+require 'showurl'
+
+def episode_exists(date)
+
+  # Convert date to show URL
+  url = show_url(date)
+
+  # Does Ze's server return 404 or 200 for this page?
+  res = Net::HTTP.start('www.zefrank.com', 80) do |http|
+    http.head(url)
+  end
+  return res.class == Net::HTTPOK
+end
+
+
+if __FILE__ == $0 then
+  require 'Date'
+  puts "There is #{episode_exists(show_date Date.today) ?"":"not "}an episode today."
+  puts "There was #{episode_exists(show_date Date.today-1) ?"":"not "}an episode yesterday."
+end
+
