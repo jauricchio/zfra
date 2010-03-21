6f810e8a9bbcef307a2d443cac82863e18102554
Joe Auricchio <jauricchio@gmail.com>
2010-03-21 15:37:52 -0700

    Driver to generate atom for today's show

---
+++ todaysshow.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'episodeexists'
+require 'showdate'
+require 'showdescription'
+require 'showurl'
+
+class Show
+  def initialize(today)
+    d = show_date(today)
+
+    if(!episode_exists(d)) then
+      exit
+    end
+
+    @title = "The Show for #{d.strftime('%A, %b %e')}"
+    @summary = show_description(d)
+    @link = show_url(d)
+    @mdy = d.strftime('%m%d%y')
+    @tag_ymd = today.strftime('%Y-%m-%d')
+
+    @updated = Time.now.xmlschema
+
+  end
+
+  def to_atom
+    erb = ERB.new(File.read('entry.erb'))
+    erb.result(binding)
+  end
+end
+
+
+if __FILE__ == $0 then
+  show = Show.new(Date.today+1)
+  puts show.to_atom
+end
+
